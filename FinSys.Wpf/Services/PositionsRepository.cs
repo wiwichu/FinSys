c034a1595c224625565ff4d15f9497852540f43a
wiwichu <mckeonppatrick@hotmail.com>
2015-09-22 14:28:39 -0400

    Positions

--- FinSys.Wpf/Services/PositionsRepository.cs
+++ FinSys.Wpf/Services/PositionsRepository.cs
@@ -12,21 +12,96 @@ namespace FinSys.Wpf.Services
         static List<Position> positions = new List<Position>();
         static PositionsRepository()
         {
-            Position a = new Position
+            Position a1 = new Position
             {
                 Portfolio = "Porta",
                 Instrument = "Instr1",
                 Amount = 10000,
                 Price = .90
             };
-            positions.Add(a);
+            Position b1 = new Position
+            {
+                Portfolio = "Portb",
+                Instrument = "Instr2",
+                Amount = 20000,
+                Price = 1.10
+            };
+            Position a2 = new Position
+            {
+                Portfolio = "Porta",
+                Instrument = "Instr2",
+                Amount = 10000,
+                Price = .90
+            };
+            Position b2 = new Position
+            {
+                Portfolio = "Portb",
+                Instrument = "Instr1",
+                Amount = 20000,
+                Price = 1.10
+            };
+            Position a3 = new Position
+            {
+                Portfolio = "Porta",
+                Instrument = "Instr3",
+                Amount = 10000,
+                Price = .90
+            };
+            Position b3 = new Position
+            {
+                Portfolio = "Portb",
+                Instrument = "Instr3",
+                Amount = 20000,
+                Price = 1.10
+            };
+            Position a4 = new Position
+            {
+                Portfolio = "Porta",
+                Instrument = "Instr4",
+                Amount = 10000,
+                Price = .90
+            };
+            Position b4 = new Position
+            {
+                Portfolio = "Portb",
+                Instrument = "Instr4",
+                Amount = 20000,
+                Price = 1.10
+            };
+            Position a5 = new Position
+            {
+                Portfolio = "Porta",
+                Instrument = "Instr5",
+                Amount = 10000,
+                Price = .90
+            };
+            Position b5 = new Position
+            {
+                Portfolio = "Portb",
+                Instrument = "Instr5",
+                Amount = 20000,
+                Price = 1.10
+            };
+            positions.Add(a1);
+            positions.Add(b1);
+            positions.Add(a2);
+            positions.Add(b2);
+            positions.Add(a3);
+            positions.Add(b3);
+            positions.Add(a4);
+            positions.Add(b4);
+            positions.Add(a5);
+            positions.Add(b5);
+
         }
         public async Task<List<Position>> GetPositionsAsync()
         {
             List<Position> pos = await Task.Run(() =>
             {
                 return positions;
-            }).ConfigureAwait(false);
+            })
+            .ConfigureAwait(false) //necessary on UI Thread
+            ;
             return pos;
         }
     }
--- FinSys.Wpf/ViewModel/TradingViewModel.cs
+++ FinSys.Wpf/ViewModel/TradingViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FinSys.Wpf.ViewModel
@@ -15,9 +16,42 @@ namespace FinSys.Wpf.ViewModel
     {
         private ObservableCollection<Position> positions = new ObservableCollection<Position>();
         private IPositionsRepository positionsRepo = RepositoryFactory.Positions;
+        private ManualResetEvent initialized = new ManualResetEvent(false);
         public TradingViewModel()
         {
-            positions = new ObservableCollection<Position>(positionsRepo.GetPositionsAsync().Result);
+            //positions = new ObservableCollection<Position>(positionsRepo.GetPositionsAsync().Result);       
+            Task.Run(() => // avoids blocking UI thread.
+            {
+                positions = new ObservableCollection<Position>(positionsRepo.GetPositionsAsync().Result);
+                initialized.Set();   
+            });     
         }
+        public ObservableCollection<Position> Positions
+        {
+            get { return positions; }
+            set
+            {
+                positions = value;
+                OnPropertyChanged();
+            }
+        }
+        object _SelectedPosition;
+        public object SelectedPosition
+        {
+            get
+            {
+                return _SelectedPosition;
+            }
+            set
+            {
+                if (_SelectedPosition != value)
+                {
+                    _SelectedPosition = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int SelectedPositionIndex { get; set; }
+
     }
 }
