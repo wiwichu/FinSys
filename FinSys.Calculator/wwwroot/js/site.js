d81f016fae523955319523d763b4aca665fb05cd
wiwichu <mckeonppatrick@hotmail.com>
2016-01-17 22:51:46 -0500

    Calculator Web Site

---
+++ FinSys.Calculator/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿//site.js
+(function () {
+
+//var ele = $("#menuname");
+//ele.text("Main Menu");
+
+//var main = $("#main");
+//main.on("mouseenter", function () {
+//    main.style = "background-color: #888;";
+//});
+//main.on("mouseleave" , function(){
+//    main.style="";
+//});
+//var menuItems = $("ul.menu li a");
+//    menuItems.on("click",function()
+//    {
+//        var me = $(this);
+//        alert(me.text());
+    //    })
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar"))
+        {
+            $(this).text("Show Sidebar");
+        }
+        else
+        {
+            $(this).text("Hide Sidebar");
+        }
+    })
+})();
