52ed4feca806676f2ededd4b39a369b9fd4498c2
wiwichu <mckeonppatrick@hotmail.com>
2016-01-19 21:33:10 -0500

    Add missing files

---
+++ FinSys.Calculator/Models/InstrumentClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinSys.Calculator.Models
+{
+    public class InstrumentClass
+    {
+        public string Name { get; set; }
+    }
+}
