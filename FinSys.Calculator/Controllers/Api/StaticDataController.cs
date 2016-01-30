a03d002c4d9525afe3ec44ad95d85b4c20c54386
wiwichu <mckeonppatrick@hotmail.com>
2016-01-30 00:39:56 -0500

    Start Api

---
+++ FinSys.Calculator/Controllers/Api/StaticDataController.cs
@@ -0,0 +1,30 @@
+﻿using FinSys.Calculator.Services;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinSys.Calculator.Controllers.Api
+{
+    public class StaticDataController : Controller
+    {
+        private ICalculatorRepository _repository;
+        public StaticDataController(ICalculatorRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet("api/staticdata")]
+        public async Task<JsonResult> Get()
+        {
+            
+            var instrumentClasses = await _repository.GetInstrumentClassesAsync();
+            var dayCounts = await _repository.GetDayCountsAsync();
+            IDictionary<string,IEnumerable<object>> staticData = new Dictionary<string, IEnumerable<object>>();
+            staticData.Add("instrumentClasses",instrumentClasses.Select((c)=>c.Name));
+            staticData.Add("dayCounts",dayCounts);
+
+            return Json(staticData);
+        }
+    }
+}
