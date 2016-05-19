933975b07649979ab3dc6be9681355028af13073
wiwichu <mckeonppatrick@hotmail.com>
2016-05-19 10:17:30 -0400

    vcremote

---
+++ FinSys.Mobile.Calc/FinSys.Mobile.Calc.Shared/FinSys_Mobile_Calc.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class FinSys_Mobile_Calc {
+public:
+    static char * getTemplateInfo();
+    FinSys_Mobile_Calc();
+    ~FinSys_Mobile_Calc();
+};
---
+++ FinSys.Mobile/FinSys.Mobile/Services/CalculatorRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSys.Mobile.Services
+{
+    public class CalculatorRepository : ICalculatorRepository
+    {
+        private List<string> classes = new List<string>();
+        public CalculatorRepository()
+        {
+            classes = GetInstrumentClassesAsync().Result;
+        }
+        public async Task<List<string>> GetInstrumentClassesAsync()
+        {
+            List<string> result = await Task.Run(() =>
+            {
+                if (classes.Count > 0)
+                {
+                    return classes;
+                }
+                List<string> instrumentClasses = new List<string>()
+                {
+                    "German Bund",
+                    "Japan Gov",
+                    "UK Gilt",
+                    "UK CD",
+                    "UK Discount",
+                    "US CD",
+                    "US Discount",
+                    "US TBOND",
+                    "Commercial Paper",
+                    "Finanzierungsschatz",
+                    "U-Schatz",
+                    "Eurobond",
+                    "MBS"
+                };
+                return instrumentClasses;
+            })
+            .ConfigureAwait(false) //necessary on UI Thread
+            ;
+            return result;
+        }
+
+    }
+
+}
+
