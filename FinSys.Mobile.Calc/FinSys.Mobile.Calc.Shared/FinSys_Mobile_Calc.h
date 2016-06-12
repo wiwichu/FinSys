21402f94005726d1921b13b611a2c5a4fb2b7611
wiwichu <mckeonppatrick@hotmail.com>
2016-06-12 08:59:01 -0400

    use calc lib

--- FinSys.Mobile.Calc/FinSys.Mobile.Calc.Shared/FinSys_Mobile_Calc.h
+++ FinSys.Mobile.Calc/FinSys.Mobile.Calc.Shared/FinSys_Mobile_Calc.h
@@ -3,6 +3,7 @@
 class FinSys_Mobile_Calc {
 public:
     static char * getTemplateInfo();
+	static char**  getclassdescriptions_internal(int& size);
     FinSys_Mobile_Calc();
     ~FinSys_Mobile_Calc();
 };
---
+++ FinSys.Mobile/FinSys.Mobile/ILibApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSys.Mobile
+{
+    public interface ILibApi
+    {
+        IntPtr getClassDescriptions(out int size);
+        string getIOSInfo();
+    }
+}
